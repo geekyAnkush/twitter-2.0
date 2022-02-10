cf001ccee538bf4ae6f1224dd3e0d82db320319c
Ankush <000.kumarankush@gmail.com>
2022-02-10 11:46:51 +0530

    second commit

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -10,6 +10,7 @@ export default NextAuth({
     }),
     // ...add more providers here
   ],
+  secret: process.env.JWT_SECRET,
   callbacks: {
     async session({ session, token }) {
       session.user.tag = session.user.name
